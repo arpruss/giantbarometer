73ac8d04a495d5c931e44a77e8c42b978ac27d8b
Alexander Pruss <arpruss@gmail.com>
2025-01-06 23:58:16 -0600

    foo

---
+++ app/build/generated/source/buildConfig/debug/mobi/omegacentauri/giantbarometer/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package mobi.omegacentauri.giantbarometer;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "mobi.omegacentauri.giantbarometer";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 108;
+  public static final String VERSION_NAME = "1.08";
+}
